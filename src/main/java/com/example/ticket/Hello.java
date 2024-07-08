08ecc5d4136a469df3e94a3bdf2d029fcb8f40f4
pankj <pankj@ptcservice.com.cn>
2024-07-08 11:51:34 +0800

    update Hello.java

--- src/main/java/com/example/ticket/Hello.java
+++ src/main/java/com/example/ticket/Hello.java
@@ -3,5 +3,6 @@ package com.example.ticket;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("helqeqwwqeqweeqwqwelo");
+        System.out.println("123");
     }
 }
