925a899d9385bc40a1078c27c339085ed894c42e
yuan.ji@saicec.com <yuan.ji@saicec.com>
2024-07-10 13:50:33 +0800

    update Hello.java

--- src/main/java/com/example/ticket/Hello.java
+++ src/main/java/com/example/ticket/Hello.java
@@ -3,6 +3,6 @@ package com.example.ticket;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("helqeqwwqeqweeqwqwelo");
-        System.out.println("123");
+        System.out.println("123321123132132");
     }
 }
